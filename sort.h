92f4e263dc715891603010291d10f0c297615c33
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-09 07:37:31 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -23,4 +23,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sorty(int *array, int *p_array, size_t size, size_t);
 #endif
